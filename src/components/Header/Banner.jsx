2b5a185ded5ae74f9c94d474ef511921fcf1aed1
riaurko <riaurko@gmail.com>
2025-04-21 15:54:18 +0600

    Built Banner
    Added Banner section

---
+++ src/components/Header/Banner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Banner() {
+    return (
+        <section id="banner" className="mx-40 mt-2 px-36 py-56 bg-[linear-gradient(to_bottom,#0004,#000A),url(https://i.ibb.co.com/6cTbSB2r/banner-img.png)] bg-cover bg-center rounded-3xl space-y-8">
+            <h1 className="text-5xl font-extrabold text-center w-3/4 mx-auto">Your Gateway to Legal Help — Fast, Easy & Reliable. Justice has Ease now.</h1>
+            <p className="font-medium text-center mx-auto w-5/6 text-lg">JustEase lets you find and book trusted lawyers across Bangladesh in just a few clicks. Whether it's personal, corporate, or urgent matters — we've got your back. Say goodbye to legal stress and hello to smooth appointments.</p>
+        </section>
+    )
+}
